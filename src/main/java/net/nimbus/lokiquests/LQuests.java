6a9c90246a164d06cf9ca7ecc649e8d29b7627dc
BuildTools <unconfigured@null.spigotmc.org>
2024-02-15 18:20:33 +0200

    asd

--- src/main/java/net/nimbus/lokiquests/LQuests.java
+++ src/main/java/net/nimbus/lokiquests/LQuests.java
@@ -1,17 +1,119 @@
 package net.nimbus.lokiquests;
 
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.InputStreamReader;
 
 public class LQuests extends JavaPlugin {
 
     public static LQuests a;
 
+
+    private YamlConfiguration messages;
+
+
+
+
+
+    public void loadConfig(boolean reload){
+        File config = new File(getDataFolder(), "config.yml");
+        if (!config.exists()) {
+            getConfig().options().copyDefaults(true);
+            saveDefaultConfig();
+            try {
+                getConfig().load(config);
+                getLogger().info("Created new config.yml file at " + config.getPath());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (reload) {
+            try {
+                getConfig().load(config);
+                getLogger().info("Config reloaded.");
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+        //Vars.init();
+    }
+
+    void loadEvents(){
+
+    }
+    void loadCommands(){
+
+    }
+
     public void onEnable() {
         a = this;
+
+        loadConfig(false);
+        loadMessages();
+
+        loadEvents();
+        loadCommands();
+
 
     }
 
     public void onDisable() {
 
+    }
+
+    void loadEvent(Listener listener){
+        try {
+            getServer().getPluginManager().registerEvents(listener, this);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void loadCommand(String cmd, CommandExecutor executor){
+        try {
+            getCommand(cmd).setExecutor(executor);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    void loadCommand(String cmd, CommandExecutor executor, TabCompleter completer){
+        try {
+            loadCommand(cmd, executor);
+            getCommand(cmd).setTabCompleter(completer);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void loadMessages(){
+        File messagesFile = new File(getDataFolder(), "messages.yml");
+        if (!messagesFile.exists()) {
+            if(!messagesFile.getParentFile().exists()) messagesFile.getParentFile().mkdirs();
+            try {
+                messages = YamlConfiguration.loadConfiguration(new InputStreamReader(getResource("messages.yml")));
+                messages.save(messagesFile);
+                getLogger().info("Created new messages.yml file at " + messagesFile.getPath());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                messages = YamlConfiguration.loadConfiguration(messagesFile);
+                getLogger().info("Messages file loaded.");
+            } catch (Exception exception) {
+            }
+        }
+    }
+
+    public YamlConfiguration getMessages() {
+        return messages;
+    }
+
+    public String getMessage(String key) {
+        return getMessages().getString(key);
     }
 }
