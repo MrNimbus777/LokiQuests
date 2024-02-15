2a64a8c74800ad268fee397ed25fdf14d9db14d8
BuildTools <unconfigured@null.spigotmc.org>
2024-02-15 18:09:25 +0200

    asd

---
+++ src/main/java/net/nimbus/lokiquests/LQuests.java
@@ -0,0 +1,17 @@
+package net.nimbus.lokiquests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LQuests extends JavaPlugin {
+
+    public static LQuests a;
+
+    public void onEnable() {
+        a = this;
+
+    }
+
+    public void onDisable() {
+
+    }
+}
